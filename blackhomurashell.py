d7d77a0d5de2465af5866a1fb03094e6cd428197
diegorego514-ship-it <diegorego514@gmail.com>
2025-12-25 19:44:44 -0300

    Add files via upload

---
+++ blackhomurashell.py
@@ -0,0 +1,15 @@
+int def {S1 == S2}
+ifndef == S1 = S2:
+find == NetSys25:
+if: paging >> define
+sys.exit(1)
+sys.exit(2)
+system.argv
+system.shell
+system.execve
+system.state.on.sys64
+sys_init_on = 0
+system.initrd64.sys
+if.comsys32 >> sys64
+import sys32
+import os
